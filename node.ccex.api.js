36d02d958931e3b8a051db44880398f6567e86dc
Infernoman <jjaauto@hotmail.com>
2016-12-27 22:40:43 -0500

    Update node.ccex.api.js

--- node.ccex.api.js
+++ node.ccex.api.js
@@ -72,3 +72,50 @@ var NodeCcexApi = function() {
         for (i = 0; i < o.length; i++) {
             uri = updateQueryStringParameter(uri, o[i], options[o[i]]);
         }
+
+        op.headers.apisign = hmac_sha512.HmacSHA512(uri, opts.apisecret); // setting the HMAC hash `apisign` http header
+        op.uri = uri;
+
+        return op;
+    };
+
+    var updateQueryStringParameter = function(uri, key, value) {
+
+        var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+        var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+
+        if (uri.match(re)) {
+            uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+        } else {
+            uri = uri + separator + key + "=" + value;
+        }
+
+        return uri;
+    };
+
+    var sendRequestCallback = function(callback, op) {
+        start = Date.now();
+        request(op, function(error, result, body) {
+            if (!body || !result || result.statusCode != 200) {
+                console.error(error);
+            } else {
+                callback(((opts.cleartext) ? body : JSON.parse(body)));
+                ((opts.verbose) ? console.log("requested from " + result.request.href + " in: %ds", (Date.now() - start) / 1000) : '');
+            }
+        });
+    };
+
+    return {
+        options: function(options) {
+            extractOptions(options);
+        },
+        custom: function(options, callback) {
+            var op = setRequestUriGetParams(apiCredentials(opts.baseUrl), options);
+            sendRequestCallback(callback, op);
+        }
+
+    };
+
+}();
+
+module.exports = NodeCcexApi;
